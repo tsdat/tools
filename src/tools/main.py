86688b2636dc684e72ebd3201dcfab6cdc3bfec8
Maxwell Levin <24307537+maxwelllevin@users.noreply.github.com>
2024-07-11 23:00:23 -0700

    Start excel2yaml upgrades

--- src/tools/main.py
+++ src/tools/main.py
@@ -1,12 +1,20 @@
 import typer
-from .data2yaml import from_data
+
 from ._version import __version__
+from .data2yaml import from_data
+from .excel2yaml import config_file_converter
 
 app = typer.Typer(add_completion=False)
+
 app.command(
     name="data2yaml",
     help="Use your data file to generate tsdat configuration files.",
 )(from_data)
+
+app.command(
+    name="excel2yaml",
+    help="Generate configuration files from an excel file.",
+)(config_file_converter)
 
 
 @app.command()
